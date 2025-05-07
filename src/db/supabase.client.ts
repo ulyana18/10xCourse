6c9e878a5ed030e5a0ca5abda2690078652e5bd8
Ulyana Bychkovskaya <ubychkovskaya20@gmail.com>
2025-05-07 09:50:08 +0200

    Add default user id

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,3 +6,7 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey); 
+
+export type SupabaseClient = typeof supabaseClient;
+
+export const DEFAULT_USER_ID = "f25d1fd3-f04f-40ac-aa5f-827c0d66d1de";
